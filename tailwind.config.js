626c7f695db8d39e829a5854772c4c1ef1d340f7
annechh <142426482+annechh@users.noreply.github.com>
2024-11-08 19:14:09 +0100

    Style: adds light mode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./**/**.{html,js}', '!./node_modules/**/*'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         purpleDark: '#150516',
         purpleLight: '#311C36',
         white: '#d9d9d9',
+        greyLight: '#9A9A9A',
+        violet: '#5B4B5C',
         whiteFaded: 'rgba(217, 217, 217, 0.25)',
       },
       dropShadow: {
