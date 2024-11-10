ca77a617cea486ad5440cb36a4d03dfc71b87ba2
annechh <142426482+annechh@users.noreply.github.com>
2024-11-10 02:57:23 +0100

    Refactor: moved classes from html files to dynamically add them

--- src/js/ui/dom/styleElements.js
+++ src/js/ui/dom/styleElements.js
@@ -1,3 +1,22 @@
+export function formDivWrapper() {
+  const formDiv = document.querySelector('.form-div-wrapper');
+  formDiv.classList.add(
+    'bg-purpleLight',
+    'dark:bg-violetLight',
+    'py-10',
+    'md:py-20',
+    'max-w-[800px]',
+    'w-full',
+    'flex',
+    'flex-col',
+    'gap-5',
+    'justify-center',
+    'items-center',
+    'rounded-xl',
+    'mx-4'
+  );
+}
+
 export function formStyle() {
   const form = document.querySelector('.form');
   form.classList.add('flex', 'flex-col', 'gap-4', 'max-w-lg', 'w-full');
@@ -7,13 +26,27 @@ export function labelInputDivStyle() {
   const targetDiv = document.querySelectorAll('.label-input-div');
 
   targetDiv.forEach((div) => {
-    div.classList.add('flex', 'flex-col', 'gap-2', 'mx-[10px]');
+    div.classList.add('flex', 'flex-col', 'gap-2', 'mx-4');
   });
 }
 
 export function labelStyle() {}
 
-export function inputStyle() {}
+export function inputStyle() {
+  const targetInput = document.querySelectorAll('.input');
+  console.log(targetInput);
+
+  targetInput.forEach((input) => {
+    input.classList.add(
+      'rounded',
+      'h-12',
+      'px-4',
+      'text-purpleDark',
+      'font-semibold',
+      'bg-white'
+    );
+  });
+}
 
 export function bodyStyle() {}
 
