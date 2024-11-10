c2bd2a493633871478a53765297cf90045ed9800
annechh <142426482+annechh@users.noreply.github.com>
2024-11-10 02:51:40 +0100

    Comment out

--- src/js/ui/dom/footer.js
+++ src/js/ui/dom/footer.js
@@ -1,17 +1,17 @@
-export function mobileFooter() {
-  const bottomItem = document.getElementById('mobileFooter');
+// export function mobileFooter() {
+//   const bottomItem = document.getElementById('mobileFooter');
 
-  window.addEventListener('scroll', () => {
-    const scrollPosition = window.innerHeight + window.scrollY;
-    const pageHeight = document.documentElement.scrollHeight;
+//   window.addEventListener('scroll', () => {
+//     const scrollPosition = window.innerHeight + window.scrollY;
+//     const pageHeight = document.documentElement.scrollHeight;
 
-    if (scrollPosition >= pageHeight - 100) {
-      bottomItem.classList.remove('hidden');
-    } else {
-      bottomItem.classList.add('hidden');
-    }
-  });
-}
+//     if (scrollPosition >= pageHeight - 100) {
+//       bottomItem.classList.remove('hidden');
+//     } else {
+//       bottomItem.classList.add('hidden');
+//     }
+//   });
+// }
 
 export function footerStyle() {
   const footerClasses = document.querySelector('.footer');
